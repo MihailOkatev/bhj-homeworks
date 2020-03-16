ab2f43dfb95b49ae80e198be47947b7bd8f78bcc
Astario <mokatev@yandex.ru>
2020-03-17 02:07:31 +0300

    dots add

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -28,6 +28,10 @@ const dots = Array.from(document.querySelectorAll(".slider__dot"));
 dots.forEach(element => {
     element.addEventListener('click', e => {
         sliderNumber = dots.indexOf(element);
+        if(document.querySelector(".slider__dot_active") != null) {
+            document.querySelector(".slider__dot_active").classList.remove("slider__dot_active");
+        };
+        e.target.classList.add("slider__dot_active");
         document.querySelector(".slider__item_active").classList.remove("slider__item_active");
         slidersArr[sliderNumber].classList.add("slider__item_active");
     })
