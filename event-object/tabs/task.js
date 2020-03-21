a6a5118d832171e4565100069920dcbd83a7c6db
Astario <mokatev@yandex.ru>
2020-03-21 14:29:30 +0300

    object event lesson tasks done

---
+++ event-object/tabs/task.js
@@ -0,0 +1,14 @@
+"use strict";
+const tabSwitchers = Array.from(document.querySelectorAll(".tab"));
+const tabContent = document.querySelectorAll(".tab__content");
+
+tabSwitchers.forEach((elem,index) => {
+    elem.addEventListener('click', e => {
+        console.log(index);
+        e.target.closest(".tabs").querySelector(".tab_active").classList.remove("tab_active");
+        e.target.classList.add("tab_active");
+        e.target.closest(".tabs").querySelector(".tab__content_active").classList.remove("tab__content_active");
+        Array.from(tabContent)[index].classList.add("tab__content_active");
+    });
+});
+
