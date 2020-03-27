4756fe0721e30cde166a398852d2e9356ee38118
Astario <mokatev@yandex.ru>
2020-03-27 18:54:59 +0300

    done

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -10,7 +10,6 @@ rotatorsArr.forEach((elem,index) => {
 
 function adsRotate() {
     itemsBox.forEach((element,index) => {
-        element[adNum[index]].style.color = element[adNum[index]].getAttribute("data-color");
          delay = element[adNum[index]].getAttribute("data-speed");
        element[adNum[index]].classList.remove("rotator__case_active");
        adNum[index]++;
