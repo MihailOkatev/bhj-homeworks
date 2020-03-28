b62f35d3f29104852177d9187376ff78775fe620
Astario <mokatev@yandex.ru>
2020-03-28 11:25:45 +0300

    task 2 code reformated

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,33 +2,33 @@
 let selections = document.querySelectorAll(".interest__check");
 selections = Array.from(selections);
 selections.forEach(el => {
-    el.addEventListener('click', e =>{
+    el.addEventListener('click', e => {
         const children = e.target.closest(".interest").querySelectorAll(".interest__check");
-    if(e.target.checked) {
-        children.forEach(el => {
-            el.checked = true;
-        })
-    } else {
-        children.forEach(el => {
-            el.checked = false;
-        })
-    }
-    if(e.target.closest(".interests_active") != null) {
-    let listitems = Array.from(e.target.closest(".interests_active").querySelectorAll(".interest__check"));
-    console.log(listitems.some(item => item.checked));
-    if(listitems.some(item => item.checked)) {
-        e.target.closest(".interests_active").parentElement.querySelector(".interest__check").indeterminate = true;
-    } else {
-        e.target.closest(".interests_active").parentElement.querySelector(".interest__check").indeterminate = false;
-    }
-    if(listitems.every(item => item.checked )) {
-        e.target.closest(".interests_active").parentElement.querySelector(".interest__check").indeterminate = false;
-        e.target.closest(".interests_active").parentElement.querySelector(".interest__check").checked = true;
-}
-    if(listitems.every(item => item.checked === false)) {
-        e.target.closest(".interests_active").parentElement.querySelector(".interest__check").checked = false;
+        if (e.target.checked) {
+            children.forEach(el => {
+                el.checked = true;
+            })
+        } else {
+            children.forEach(el => {
+                el.checked = false;
+            })
+        }
+        if (e.target.closest(".interests_active") != null) {
+            let listitems = Array.from(e.target.closest(".interests_active").querySelectorAll(".interest__check"));
+            console.log(listitems.some(item => item.checked));
+            if (listitems.some(item => item.checked)) {
+                e.target.closest(".interests_active").parentElement.querySelector(".interest__check").indeterminate = true;
+            } else {
+                e.target.closest(".interests_active").parentElement.querySelector(".interest__check").indeterminate = false;
+            }
+            if (listitems.every(item => item.checked)) {
+                e.target.closest(".interests_active").parentElement.querySelector(".interest__check").indeterminate = false;
+                e.target.closest(".interests_active").parentElement.querySelector(".interest__check").checked = true;
+            }
+            if (listitems.every(item => item.checked === false)) {
+                e.target.closest(".interests_active").parentElement.querySelector(".interest__check").checked = false;
 
-    }
-    }
+            }
+        }
     });
-    });
+});
